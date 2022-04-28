8879d3b1df4585fa4ba7a908e1dc2cf056f933a3
oishikatta90 <xxxwon90@gmail.com>
2022-04-28 14:06:03 +0900

    디테일 추가

--- src/main/java/com/mysite/jts/question/QuestionController.java
+++ src/main/java/com/mysite/jts/question/QuestionController.java
@@ -4,6 +4,7 @@ package com.mysite.jts.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +20,13 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id")Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 
 }

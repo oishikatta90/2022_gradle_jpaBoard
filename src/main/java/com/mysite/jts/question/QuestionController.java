331a282e85a6e192efc7d6b564fb4ab7283e515a
oishikatta90 <xxxwon90@gmail.com>
2022-05-03 12:42:30 +0900

    질문등록

--- src/main/java/com/mysite/jts/question/QuestionController.java
+++ src/main/java/com/mysite/jts/question/QuestionController.java
@@ -4,8 +4,7 @@ package com.mysite.jts.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +27,17 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";  //질문 저장 후 질문목록으로 이동
     }
 
 }
